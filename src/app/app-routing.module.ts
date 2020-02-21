e5d37242d47e34f4ef26e6c3aaa877a49b60f35d
4cruzeta <4cruzetam@gmail.com>
2020-02-21 10:41:30 -0300

    Tabs adicionados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
-  },
-  {
-    path: 'places',
-    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
-  },
-
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' }
 ];
 
 @NgModule({
